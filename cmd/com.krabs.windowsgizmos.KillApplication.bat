494301987a312649c27aa2feb7b2c34cc2ecc0ec
Krabs <78506543+krabs-github@users.noreply.github.com>
2022-01-19 15:47:33 -0500

    v1.0.7 Full Release Version
    v1.0.7 Full Release Version

--- cmd/com.krabs.windowsgizmos.KillApplication.bat
+++ cmd/com.krabs.windowsgizmos.KillApplication.bat
@@ -1,3 +1,4 @@
 @echo off
 REM echo setTitle This is\nthe title
-IF "%BPS_vKrabs_ApplicationExe%"=="" ( vTestFailed=True ) ELSE (taskkill /im %BPS_vKrabs_ApplicationExe% /t %BPS_vKrabs_Force%)
+IF "%BPS_vKrabs_Force%"=="true" vForce="/f"
+IF "%BPS_vKrabs_ApplicationName%"=="" ( vTestFailed=True ) ELSE (taskkill /im %BPS_vKrabs_ApplicationName% /t %vForce%)
