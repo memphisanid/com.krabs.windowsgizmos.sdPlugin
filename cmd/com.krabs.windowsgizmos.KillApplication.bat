3abfbd76db09ae86b97fa06a1b8cd68426f4ebd7
Krabs <78506543+krabs-github@users.noreply.github.com>
2022-01-28 15:18:53 -0500

    v1.0.8
    - Fixed a type in "Close/Kill Application" action that prevented it from working properly.
    - Fixed a bug that would not close applications with spaces in the name.

--- cmd/com.krabs.windowsgizmos.KillApplication.bat
+++ cmd/com.krabs.windowsgizmos.KillApplication.bat
@@ -1,4 +1,4 @@
 @echo off
 REM echo setTitle This is\nthe title
 IF "%BPS_vKrabs_Force%"=="true" vForce="/f"
-IF "%BPS_vKrabs_ApplicationName%"=="" ( %vTestFailed%=True ) ELSE (taskkill /IM %BPS_vKrabs_ApplicationName% %vForce%)
+IF "%BPS_vKrabs_ApplicationName%"=="" ( %vTestFailed%=True ) ELSE (taskkill /IM "%BPS_vKrabs_ApplicationName%"" %vForce%)
