65c88d97c6058ff76c5a175db7a6b9c0e570da94
Krabs <78506543+krabs-github@users.noreply.github.com>
2022-11-28 18:03:39 -0500

    v1.0.17
    - HDR Toggle now toggles depending whether HDR is enabled.

--- cmd/com.krabs.windowsgizmos.hdrtoggle.bat
+++ cmd/com.krabs.windowsgizmos.hdrtoggle.bat
@@ -1,3 +1,13 @@
 @echo off
 REM echo setTitle This is\nthe title
 PowerShell.exe -ExecutionPolicy Bypass -Command "./cmd/scripts/HDRToggle.ps1"
+dxdiag /x %TEMP%\dxdiag.xml
+setlocal EnableDelayedExpansion
+set RESULT="
+set ISEMPTY="
+for /f %%i in ('findstr /c:"<AdvancedColorEnabled>1</AdvancedColorEnabled>" %TEMP%\dxdiag.xml') do set RESULT=%%i
+if "!RESULT!" == "!ISEMPTY!" ( 
+echo setState 0
+) else (
+echo setState 1
+)
