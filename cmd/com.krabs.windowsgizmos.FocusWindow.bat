5db58005a6db243506cbf89d64fed13b3820e3fa
Krabs <78506543+krabs-github@users.noreply.github.com>
2023-05-05 15:37:59 -0400

    Version: 1.0.22
    - Changes to Focus Window PI.
    - Added Focus Window images.

--- cmd/com.krabs.windowsgizmos.FocusWindow.bat
+++ cmd/com.krabs.windowsgizmos.FocusWindow.bat
@@ -1,4 +1,5 @@
 @echo off
 REM echo setTitle This is\nthe title
-PowerShell.exe -ExecutionPolicy Bypass -Command './cmd/scripts/FocusWindow.ps1 "%BPS_vKrabs_ProcessName%"'
+vProcessname="%BPS_vKrabs_ProcessName%"
+PowerShell.exe -ExecutionPolicy Bypass -Command "./cmd/scripts/FocusWindow.ps1 %BPS_vKrabs_ProcessName%"
 EXIT
